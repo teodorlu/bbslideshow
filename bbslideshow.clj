1c86f430baecaa506d5487a0cbae1187a94fc83f
Teodor Heggelund <git@teod.eu>
2024-05-09 11:39:35 +0200

    It actually works! :grin:

--- bbslideshow.clj
+++ bbslideshow.clj
@@ -31,14 +31,30 @@
   (char 103)
   (int \k))
 
-(defn navigate-loop [slides current-index]
-  (when-let [slide (get slides current-index)]
-    (dotimes [_ slide-top-padding]
-      (println))
-    (print (slurp (fs/file slide)))))
+(defn navigate-loop [slides start-at-index]
+  (loop [index start-at-index]
+    (when-let [slide (get slides index)]
+      (dotimes [_ slide-top-padding]
+        (println))
+      (print (slurp (fs/file slide)))
+      (flush)
+      (let [character (char (.read System/in))]
+        (case (get keymap character :bbslideshow/command-not-found)
+          :bbslideshow/command-not-found
+          (do
+            (prn [:bbslideshow/command-not-found {:character character}])
+            nil)
+
+          :bbslideshow/next-slide
+          (recur (inc index))
+
+          :bbslideshow/prev-slide
+          (recur (dec index))
+
+          (do (prn "command found") nil))))))
 
 (defn enable-reading-char-by-char! []
-  (process/shell "stty -icanon -echo"))
+    (process/shell "stty -icanon -echo"))
 
 (defn disable-reading-char-by-char! []
   (process/shell "stty icanon echo"))
@@ -54,9 +70,11 @@
         (disable-reading-char-by-char!)))))
 
 (defn -debug [& _args]
+  (enable-reading-char-by-char!)
   (let [k (.read System/in)]
     (prn [k (type k)])
-    (prn (get keymap (char k) :bbslideshow/command-not-found))))
+    (prn (get keymap (char k) :bbslideshow/command-not-found)))
+  (disable-reading-char-by-char!))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply (if (= "--debug" (first *command-line-args*)) -debug -main)
