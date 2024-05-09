78468ce2ce5c623a91c38d393e0bd8b73c940531
Teodor Heggelund <git@teod.eu>
2024-05-09 10:06:29 +0200

    Print the current folder

--- bbslideshow.clj
+++ bbslideshow.clj
@@ -1,3 +1,11 @@
 #!/usr/bin/env bb
 
-(prn "hello bbslideshow")
+(ns bbslideshow
+  (:require [babashka.fs :as fs]))
+
+(defn -main [& args]
+  (let [root (or (first args) ".")]
+    (prn (fs/canonicalize (fs/absolutize (fs/file root))))))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
